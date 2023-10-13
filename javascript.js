fc90efea1569f8e17d30558709451ab8a14ba653
mavu1707 <markus.yung.vullum@gmail.com>
2023-10-14 01:07:25 +0200

    Testing

--- javascript.js
+++ javascript.js
@@ -13,3 +13,21 @@ function toggleMenu() {
 }
 
 hamburgerMenu.addEventListener('click', toggleMenu);
+
+/*let hamburger= document.getElementById("hamburger")
+let fullScreenMenu = document.querySelector('.full-screen-menu')
+
+function toggleMenu() {
+  let bars = document.querySelectorAll('.bar')
+  bars.forEach(bar => bar.classList.toggle('change'))
+
+  if (fullScreenMenu.style.display === "flex") {
+    fullScreenMenu.style.display = "none"
+    hamburger.classList.remove('active')
+  } else {
+    fullScreenMenu.style.display = "flex"
+    hamburger.classList.add('active')
+  }
+}
+
+hamburger.addEventListener('click', toggleMenu)*/
